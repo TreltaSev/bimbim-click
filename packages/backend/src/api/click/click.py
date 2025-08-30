17add5144d67e64b9273d47a80cab0c6951e1999
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-30 02:32:12 -0400

    feat(clicks): Show host on get

--- packages/backend/src/api/click/click.py
+++ packages/backend/src/api/click/click.py
@@ -1,12 +1,14 @@
 # === Core ===
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
 @router.get("/click")
-async def auth():
+async def auth(request: Request):
+    if request.client:
+        print(request.client.host)
     return {"count": 29731}
 
 @router.post("/click")
-async def auth():
+async def auth(request: Request):
     return {"ok": True}
