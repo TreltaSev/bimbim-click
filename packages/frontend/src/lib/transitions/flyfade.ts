ec78f1b783a51e561931d06434ca80ac0e818e23
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-31 11:20:42 -0400

    feat(flyfade): Update implementation

--- packages/frontend/src/lib/transitions/flyfade.ts
+++ packages/frontend/src/lib/transitions/flyfade.ts
@@ -1,12 +1,39 @@
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function fallback(value: any, fallback: any) {
+    return value === undefined ? fallback : value
+}
+
+type tFlyFadeOptions = {
+    duration?: number,
+    start?: number,
+    end?: number,
+    intro?: boolean,
+}
+
+/**
+ * 
+ * @param node Element to be animated
+ * @param options
+ * @returns 
+ */
 export function flyfade(
 	node: Element,
-	{
-		duration,
-		start = 0,
-		end = 0,
-		intro = false
-	}: { duration: number; start?: number; end?: number; intro?: boolean }
+	options: tFlyFadeOptions = {
+		duration: 1000,
+		start: 0,
+		end: 0,
+		intro: false
+	}
 ) {
+
+    // Get Fallback Values
+    const duration = fallback(options.duration, 1000);
+    const start = fallback(options.start, 0);
+    const end = fallback(options.end, 0);
+    const intro = fallback(options.intro, false);
+
 	return {
 		duration,
 		css: (t: number) => {
