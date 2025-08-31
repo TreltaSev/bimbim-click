a478fcb0f2b8e1c038945610f355d1664b578eff
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-31 00:54:58 -0400

    feat(backend): sprites, map to direct logic

--- packages/backend/src/api/sprite/sprite.py
+++ packages/backend/src/api/sprite/sprite.py
@@ -1,26 +1,36 @@
 # === Core ===
 import os
 import base64
-from fastapi import APIRouter, Response
+import pathlib
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Path, Response, status
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
 @router.get("/sprites")
-async def meows(response: Response):
-    files_data = {}
-
+async def sprites(response: Response):
+    out_dirs = []
     dir_path = "/sprites"
     if os.path.exists(dir_path):
         for filename in os.listdir(dir_path):
             if filename.lower().endswith(".png"):
-                file_path = os.path.join(dir_path, filename)
-                try:
-                    with open(file_path, "rb") as f:
-                        # base64 encode the contents
-                        b64_data = base64.b64encode(f.read()).decode("utf-8")
-                        files_data[filename] = b64_data
-                except Exception as e:
-                    files_data[filename] = f"Error: {e}"
+                out_dirs.append(filename)
 
     response.status_code = 200
-    return files_data
+    return out_dirs
+
+@router.get("/sprites/{sprite_name}")
+async def sprites_query(response: Response, sprite_name: Annotated[str, Path()]):
+    
+    image_path = pathlib.Path(f"/sprites/{sprite_name}")
+    if not image_path.exists:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"Sprite with name {sprite_name} doesn't exist")
+    
+    response.media_type = "image/png"
+    
+    return FileResponse(
+        image_path,
+        media_type="image/png",
+        headers={"Content-Disposition": f"inline; filename={image_path.name}"}
+    )
