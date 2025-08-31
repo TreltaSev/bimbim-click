dc270721c7fe2c96fdc972bffdd20f02bdc8f4ee
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-31 12:15:27 -0400

    feat(remove): Iconify

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
-import { Icons } from 'unplugin-icons/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), Icons({ compiler: 'svelte', autoinstall: true })],
+	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		allowedHosts: true
 	}
