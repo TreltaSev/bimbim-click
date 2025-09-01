b41268795967bcb905b83afcdde461ee1b82e408
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-31 21:07:42 -0400

    feat(clicks): Stagnate clicks, closes #4

--- packages/backend/src/utils/abc/handlers/click.py
+++ packages/backend/src/utils/abc/handlers/click.py
@@ -15,68 +15,60 @@ from typing import ClassVar
 
 class ClickMeta(WrapperModel):
     __collection__: ClassVar[Collection] = MongoClient.clicks
-    
-    
+
     class RootModel(BaseModel):
         count: int = 0
-        
+
         class Config:
             extra = "allow"
-        
+
     class IpModel(BaseModel):
         count: int = 0
-        
+
         class Config:
             extra = "allow"
-            
+
     @property
     def root_callback(self) -> RootModel:
         """
         Returns a root pydantic model holding a count type
         """
-        
+
         response = self.__collection__.find_one({"_id": "root"})
         if response is None:
             response = {"_id": "root", "count": 0}
             self.__collection__.insert_one(response)
-                
+
         return self.RootModel(**response)
-    
+
     def get_ip(self, host: str) -> IpModel | None:
-        
+
         # Return none if invalid ip
         try:
             ipaddress.IPv4Address(host)
         except ipaddress.AddressValueError:
             return None
-        
+
         response = self.__collection__.find_one({"_id": host}) or {}
         return self.IpModel(**response)
 
+
 class Clicks:
-    
+
     def __init__(self) -> None:
-        pass
-    
+        self.delta_count = 0
+
     def addCount(self, amount: int = 1, host: str | None = None) -> None:
         """
         Updates the counter within the root document. Also if the host variable is a valid ipv4 address, iterates that as well
         """
-        self.discover.update({"_id": "root"}, "$inc", {"count": amount})
-        
-        try:
-            # If valid ip address, continue
-            ipaddress.IPv4Address(host)        
-            
-            # Insert ip count as well
-            if self.discover.exists(_id=host):
-                self.discover.update({"_id": host}, "$inc", {"count": amount})
-            else:
-                self.discover.__collection__.insert_one({"_id": host, "count": amount})
-                
-        except ipaddress.AddressValueError:
-            pass
-    
+        self.delta_count += amount
+
+    def pushCount(self) -> None:
+        self.discover.update({"_id": "root"}, "$inc", {
+                             "count": self.delta_count})
+        self.delta_count = 0
+
     @property
     def root(self) -> ClickMeta.RootModel:
         """
@@ -84,7 +76,7 @@ class Clicks:
         such as the total amount of clicks
         """
         return ClickMeta().root_callback
-    
+
     @property
     def discover(self) -> ClickMeta:
         """
@@ -92,6 +84,6 @@ class Clicks:
         who clicked or sort the top ip clicks
         """
         return ClickMeta()
-    
-clicks = Clicks()
 
+
+clicks = Clicks()
